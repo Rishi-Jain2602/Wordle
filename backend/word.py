188887b9aa516cee7f4cb6b745d0ec73c9fb8a19
RishiJain26 <rishijainai262003@gmail.com>
2024-11-30 16:59:47 +0530

    Generates Eng and hiindi word

---
+++ backend/word.py
@@ -0,0 +1,14 @@
+from nltk.corpus import wordnet as wn
+import random
+import nltk
+import requests
+nltk.download('wordnet')
+
+def get_random_english_word(length=5):
+    all_synsets = list(wn.all_synsets())
+    five_letter_words = [lemma.name() for synset in all_synsets for lemma in synset.lemmas() if len(lemma.name()) == length]
+    return random.choice(five_letter_words) if five_letter_words else None
+
+def get_random_hindi_word():
+    response = requests.get("https://random-hindi-word.onrender.com/random_hindi_word/")
+    return response
