553ddddd1b0d6f5a41bd295f193a8572a85f9b0c
RishiJain26 <rishijainai262003@gmail.com>
2024-12-14 11:22:53 +0530

    upd main backend

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
-from word import get_random_english_word,get_random_hindi_word
+from word_gen.eng_word import get_random_english_word
+from word_gen.hindi_word import get_random_hindi_word
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware  
 from pydantic import BaseModel
@@ -14,16 +15,13 @@ app.add_middleware(
     allow_headers=["*"],  
 )
 
-rand_english_word = get_random_english_word("eagle")
 game_state_english = {
     "max_attempts": 5,
     "attempts": 0,
     "game_over": False,
-    "target_word": rand_english_word[0].lower(),
-    "target_hint": rand_english_word[1].lower() + "\nOther Like: "+ rand_english_word[2][0]+" , "+rand_english_word[2][1]
 }
 
-get_random_hindi= get_random_hindi_word()
+get_random_hindi= get_random_hindi_word("जानवर")
 game_state = game_state_hindi = {
     "max_attempts": 5,
     "attempts": 0,
@@ -33,6 +31,7 @@ game_state = game_state_hindi = {
 }
 
 class UserWord(BaseModel):
+    category:str
     guess:str
     lang:str
 
@@ -40,12 +39,29 @@ class UserWord(BaseModel):
 def guess_word(user_word: UserWord):
     global game_state_english, game_state_hindi
     global game_state
+    
     guess = user_word.guess.lower()
     lang = user_word.lang
-
-    if lang == "eng":
+    category = user_word.category
+    if lang == "eng" and "target_word" not in game_state_english:
+        rand_english_word = get_random_english_word(category)  
+        game_state_english = { 
+            "max_attempts": 5,
+            "attempts": 0,
+            "game_over": False,
+            "target_word": rand_english_word[0].lower(),
+            "target_hint": rand_english_word[1].lower() + "\nSimilar words Like: " + rand_english_word[2][0] + " , " + rand_english_word[2][1]
+        }   
         game_state = game_state_english
-    else:
+    elif lang == "hin" and "target_word" not in game_state_hindi:
+        get_random_hindi= get_random_hindi_word("जानवर")
+        game_state_hindi = {
+            "max_attempts": 5,
+            "attempts": 0,
+            "game_over": False,
+            "target_word": get_random_hindi[0],
+            "target_hint":get_random_hindi[1] + "\n Similar words like: " + get_random_hindi[2][0] + "," + get_random_hindi[2][1]
+        }
         game_state = game_state_hindi
 
     if game_state["game_over"]:
@@ -68,30 +84,18 @@ def guess_word(user_word: UserWord):
 
 def generate_feedback(guess: str,lang:str, win=False):
     global game_state
-    feedback = []
-    target = list(game_state["target_word"])
-    guess_letters = list(guess)
-
-    for i in range(len(guess_letters)):
-        if guess_letters[i] == target[i]:  
-            feedback.append((guess_letters[i], "green")) 
-        else:
-            feedback.append((guess_letters[i], "gray"))
-
-    for i in range(len(guess_letters)):
-        if feedback[i][1] == "gray" and guess_letters[i] in target:
-            feedback[i] = (guess_letters[i], "yellow")    
-
-    similarity = compare_words(guess,game_state["target_word"])
+    
+    similarity = compare_words(guess,game_state["target_word"],lang)
     
     if win:
-        return {"feedback": feedback ,"Similarity":similarity, "message": "Congratulations! You've won!", "attempts": game_state["attempts"]}
+        return {"Similarity":similarity, "message": "Congratulations! You've won!", "attempts": game_state["attempts"]}
     elif game_state["game_over"]:
-        return {"feedback": feedback,"Similarity":similarity, "message": f"Game over! The word was {game_state['target_word']}.", "attempts": game_state["attempts"]}
+        return {"Similarity":similarity, "message": f"Game over! The word was {game_state['target_word']}.", "attempts": game_state["attempts"]}
     else:
-        return {"feedback": feedback , "Hint" : game_state["target_hint"],"Similarity":similarity, "message": f"{game_state['max_attempts'] - game_state['attempts']} attempts remaining."}
+        return {"Hint" : game_state["target_hint"],"Similarity":similarity, "message": f"{game_state['max_attempts'] - game_state['attempts']} attempts remaining."}
 
 class reset(BaseModel):
+    category:str
     lang:str
 
 @app.post("/wordle/reset_game")
@@ -100,23 +104,25 @@ def reset_game(user_reset:reset):
     global game_state
     lang = user_reset.lang
     if lang == "eng":
-        get_random_english = get_random_english_word("eagle")
-        game_state = game_state_english = {
+        get_random_english = get_random_english_word(user_reset.category)
+        game_state_english = {
             "max_attempts": 5,
             "attempts": 0,
             "game_over": False,
             "target_word": get_random_english[0].lower(),
-            "target_hint": get_random_english[1].lower() + "\nOther Like: "+rand_english_word[2][0]+" , "+rand_english_word[2][1]
+            "target_hint": get_random_english[1].lower() + "\nSimilar words Like: "+get_random_english[2][0]+" , "+get_random_english[2][1]
         }
+        game_state = game_state_english
     else:
-        get_random_hindi = get_random_hindi_word()
-        game_state = game_state_hindi = {
+        get_random_hindi = get_random_hindi_word("जानवर")
+        game_state_hindi = {
             "max_attempts": 5,
             "attempts": 0,
             "game_over": False,
             "target_word": get_random_hindi[0],
-            "target_hint":get_random_hindi[1] 
+            "target_hint":get_random_hindi[1] + "\n Similar words like: " + get_random_hindi[2][0] + "," + get_random_hindi[2][1]
         }
+        game_state = game_state_hindi
     return {"message": "Game reset successfully. A new word has been chosen."}
 
 
