cca993141fde294e8b10cb6341c68a34aefc34b1
RishiJain26 <rishijainai262003@gmail.com>
2024-12-14 11:26:50 +0530

    sep: end_word file

---
+++ backend/word_gen/eng_word.py
@@ -0,0 +1,33 @@
+from nltk.corpus import wordnet as wn
+import random
+import nltk
+import requests
+nltk.download('wordnet')
+
+def description_word_eng(word):
+    synsets = wn.synsets(word)
+    if synsets:
+        return synsets[0].definition()
+    else:
+        return None
+
+def get_random_english_word(category):
+    try:
+        synsets = wn.synsets(category)
+        if synsets:
+            hyponyms = []
+            for synset in synsets:
+                for hyponym in synset.hyponyms():
+                  for lemma in hyponym.lemmas():
+                    hyponyms.append(lemma.name())
+            sampled_hyponym = random.sample(hyponyms,1)[0]
+            other_hypo = random.sample([h for h in hyponyms if h != sampled_hyponym], 4)
+            description = description_word_eng(sampled_hyponym)
+            result = [sampled_hyponym, description, other_hypo]
+            return result
+        else:
+            return None
+    except Exception as e:
+      print(f"An error occurred: {e}")
+      return None
+
