d30a2fe65ad548e3920def431a4cbe8ba3aef23c
Rishi-Jain2602 <rj1016743@gmail.com>
2025-01-17 22:56:32 +0530

    upd instruction

--- frontend/src/components/Instructions.js
+++ frontend/src/components/Instructions.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './styles/Instructions.css'; 
+import './styles/Instructions.css';
 
 export default function Instructions() {
   return (
@@ -8,20 +8,23 @@ export default function Instructions() {
 
       <div className="instructions-content">
         <h2 className="instruction-item">
-          - Choose your preferred language (English, Hindi, or any).
+          1. Choose your preferred language (English, Hindi, or any).
         </h2>
         <h2 className="instruction-item">
-          - Enter a word in the selected language and category.
+          2. Enter a word in the selected language and category.
         </h2>
         <h2 className="instruction-item">
-          - Click the Submit button, and the game will provide feedback:
+          3. Click the Submit button, and the game will provide feedback:
         </h2>
-        <h2 className="instruction-item">
+        <h2 className="instruction-item mx-5">
           - Similarity score (ranges from 0 to 1).
+        </h2>
+        <h2 className="instruction-item mx-5">
           - Common keywords between your guess and the target word.
         </h2>
+
         <h2 className="instruction-item">
-          - You have a total of 5 attempts to guess the word correctly.
+          4. You have a total of 5 attempts to guess the word correctly.
         </h2>
       </div>
     </div>
